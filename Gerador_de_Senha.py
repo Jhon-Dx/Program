dc6b6de2364a2967051e0b813f62c76bff707cb5
Jhon-Dx <83149315+Jhon-Dx@users.noreply.github.com>
2021-05-11 01:00:44 -0300

    att

--- Gerador_de_Senha.py
+++ Gerador_de_Senha.py
@@ -1,1 +1,20 @@
-print("Ola Mundo")
+import PySimpleGUI as sg
+
+
+class TelaPython:
+    def __init__(self):
+        layout = [
+            [sg.Text('Nome:'), sg.Input()],
+            [sg.Text('Idade:'), sg.Input()],
+            [sg.Button('Enviar Dados')]
+        ]
+        janela = sg.Window("Dados do Usuario").layout(layout)
+        self.button, self.values = janela.read()
+
+
+    def Iniciar(self):
+        print(self.values)
+
+
+tela = TelaPython()
+tela.Iniciar()
